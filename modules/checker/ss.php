6fd774408868319bf1a2c71a3bd851f9bccfc473
iNaveen <67575446+iam-NVN@users.noreply.github.com>
2021-09-21 10:44:34 +0530

    Update ss.php

--- modules/checker/ss.php
+++ modules/checker/ss.php
@@ -62,8 +62,6 @@ if(strpos($message, "/ss ") === 0 || strpos($message, "!ss ") === 0){
         
             $ch = curl_init();
             curl_setopt($ch, CURLOPT_URL, 'https://m.stripe.com/6');
-            curl_setopt($ch, CURLOPT_PROXY, $url[array_rand($url)]);
-            curl_setopt($ch, CURLOPT_PROXYUSERPWD, $userpass[array_rand($userpass)]);
             curl_setopt($ch, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']);
             curl_setopt($ch, CURLOPT_HEADER, 0);
             curl_setopt($ch, CURLOPT_HTTPHEADER, array(
