0fda7b827623109d1bac51a62023cfb3965c9e9a
Mabidax <67502330+Mabidax@users.noreply.github.com>
2022-05-07 17:45:40 +0200

    Update schk.php

--- modules/checker/schk.php
+++ modules/checker/schk.php
@@ -150,7 +150,7 @@ Time -» <b>$time</b><b>s</b>
 <b>----------------------------</b>
 
 <b>Checked By <a href='tg://user?id=$userId'>$firstname</a></b>
-<b>Bot By: <a href='t.me/ninjanaveen'>ɴɪɴᴊᴀ ɴᴀᴠᴇᴇɴ</a></b>",
+<b>Bot By: <a href='t.me/Mabidax_The_Lost_Noob'>TheLostNooB</a></b>",
                     'parse_mode'=>'html',
                     'disable_web_page_preview'=>'true'
                     
@@ -218,7 +218,7 @@ Time -» <b>$time</b><b>s</b>
 <b>----------------------------</b>
 
 <b>Checked By <a href='tg://user?id=$userId'>$firstname</a></b>
-<b>Bot By: <a href='t.me/ninjanaveen'>ɴɪɴᴊᴀ ɴᴀᴠᴇᴇɴ</a></b>",
+<b>Bot By: <a href='t.me/Mabidax_The_Lost_Noob'>TheLostNooB</a></b>",
                 'parse_mode'=>'html',
                 'disable_web_page_preview'=>'true'
                 
@@ -247,7 +247,7 @@ Time -» <b>$time</b><b>s</b>
 <b>----------------------------</b>
 
 <b>Checked By <a href='tg://user?id=$userId'>$firstname</a></b>
-<b>Bot By: <a href='t.me/ninjanaveen'>ɴɪɴᴊᴀ ɴᴀᴠᴇᴇɴ</a></b>",
+<b>Bot By: <a href='t.me/Mabidax_The_Lost_Noob'>TheLostNooB</a></b>",
                 'parse_mode'=>'html',
                 'disable_web_page_preview'=>'true'
                 
