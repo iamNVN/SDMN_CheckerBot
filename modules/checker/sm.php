719cfcead36624f6f2157c215690d057b19d0f9c
Naveen Kumar <67575446+iam-NVN@users.noreply.github.com>
2023-03-29 19:40:54 +0530

    Update sm.php

--- modules/checker/sm.php
+++ modules/checker/sm.php
@@ -141,7 +141,7 @@ Time -» <b>$time</b><b>s</b>
 <b>----------------------------</b>
 
 <b>Checked By <a href='tg://user?id=$userId'>$firstname</a></b>
-<b>Bot By: <a href='t.me/ninjanaveen'>ɴɪɴᴊᴀ ɴᴀᴠᴇᴇɴ</a></b>",
+<b>Bot By: <a href='t.me/iamNVN'>ɴɪɴᴊᴀ ɴᴀᴠᴇᴇɴ</a></b>",
                     'parse_mode'=>'html',
                     'disable_web_page_preview'=>'true'
                     
@@ -212,7 +212,7 @@ Time -» <b>$time</b><b>s</b>
 <b>----------------------------</b>
 
 <b>Checked By <a href='tg://user?id=$userId'>$firstname</a></b>
-<b>Bot By: <a href='t.me/ninjanaveen'>ɴɪɴᴊᴀ ɴᴀᴠᴇᴇɴ</a></b>",
+<b>Bot By: <a href='t.me/iamNVN'>ɴɪɴᴊᴀ ɴᴀᴠᴇᴇɴ</a></b>",
                 'parse_mode'=>'html',
                 'disable_web_page_preview'=>'true'
                 
@@ -241,7 +241,7 @@ Time -» <b>$time</b><b>s</b>
 <b>----------------------------</b>
 
 <b>Checked By <a href='tg://user?id=$userId'>$firstname</a></b>
-<b>Bot By: <a href='t.me/ninjanaveen'>ɴɪɴᴊᴀ ɴᴀᴠᴇᴇɴ</a></b>",
+<b>Bot By: <a href='t.me/iamNVN'>ɴɪɴᴊᴀ ɴᴀᴠᴇᴇɴ</a></b>",
                 'parse_mode'=>'html',
                 'disable_web_page_preview'=>'true'
                 
